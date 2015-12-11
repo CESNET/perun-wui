f1639ab738391b5589675610db00afa690fcc16a
Pavel Zlámal <zlamal@cesnet.cz>
2015-12-11 08:14:12 +0100

    WUI: Registrar can now verify email address from application
    - Implemented page for mail verification compatible with old links for registrar.

--- perun-wui-registrar/src/main/java/cz/metacentrum/perun/wui/registrar/client/PerunRegistrarPresenter.java
+++ perun-wui-registrar/src/main/java/cz/metacentrum/perun/wui/registrar/client/PerunRegistrarPresenter.java
@@ -1,15 +1,14 @@
 package cz.metacentrum.perun.wui.registrar.client;
 
 import com.google.gwt.core.client.*;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
 import com.google.web.bindery.event.shared.EventBus;
 import com.gwtplatform.mvp.client.View;
 import com.gwtplatform.mvp.client.annotations.ProxyStandard;
+import com.gwtplatform.mvp.client.proxy.PlaceManager;
 import com.gwtplatform.mvp.client.proxy.Proxy;
+import com.gwtplatform.mvp.shared.proxy.PlaceRequest;
 import cz.metacentrum.perun.wui.client.PerunPresenter;
 import cz.metacentrum.perun.wui.client.resources.PerunSession;
 import cz.metacentrum.perun.wui.client.utils.JsUtils;
@@ -18,9 +17,6 @@ import cz.metacentrum.perun.wui.json.JsonEvents;
 import cz.metacentrum.perun.wui.json.managers.RegistrarManager;
 import cz.metacentrum.perun.wui.model.PerunException;
 import cz.metacentrum.perun.wui.model.beans.Attribute;
-import cz.metacentrum.perun.wui.model.common.PerunPrincipal;
-import cz.metacentrum.perun.wui.registrar.model.RegistrarObject;
-import cz.metacentrum.perun.wui.widgets.PerunLoader;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -30,110 +26,124 @@ import java.util.List;
  * @author Ondrej Velisek <ondrejvelisek@gmail.com>
  */
 public class PerunRegistrarPresenter extends PerunPresenter<PerunRegistrarPresenter.MyView, PerunRegistrarPresenter.MyProxy> {
-    @ProxyStandard
-    public interface MyProxy extends Proxy<PerunRegistrarPresenter> {
-    }
 
-    public interface MyView extends View {
+	@ProxyStandard
+	public interface MyProxy extends Proxy<PerunRegistrarPresenter> {
+	}
 
-        void onLoadingStartFooter();
+	public interface MyView extends View {
 
-        /**
-         * Show list of contact emails
-         * @param contactEmail null if there was an error meanwhile loading data List otherwise.
-         */
-        void onFinishedFooter(List<String> contactEmail);
+		void onLoadingStartFooter();
 
-        void hideNavbar();
-    }
+		/**
+		 * Show list of contact emails
+		 * @param contactEmail null if there was an error meanwhile loading data List otherwise.
+		 */
+		void onFinishedFooter(List<String> contactEmail);
 
-    @Inject
-    PerunRegistrarPresenter(EventBus eventBus, MyView view, MyProxy proxy) {
-        super(eventBus, view, proxy);
-    }
+		void hideNavbar();
+	}
 
-    @Override
-    protected void onBind() {
-        super.onBind();
+	private PlaceManager placeManager = PerunSession.getPlaceManager();
 
-        loadVoAttributes(new Events<List<Attribute>>() {
+	@Inject
+	PerunRegistrarPresenter(EventBus eventBus, MyView view, MyProxy proxy) {
+		super(eventBus, view, proxy);
+	}
 
-            @Override
-            public void onFinished(List<Attribute> result) {
+	@Override
+	protected void onBind() {
+		super.onBind();
 
-                Attribute contactEmailAttr = null;
-                for (Attribute attr : result) {
-                    if (attr.getFriendlyName().equals("contactEmail")) {
-                        contactEmailAttr = attr;
-                        break;
-                    }
-                }
-                List<String> contactEmails = new ArrayList<>();
-                if (contactEmailAttr != null) {
-                    contactEmails = Arrays.asList(contactEmailAttr.getValue().split(","));
-                }
+		loadVoAttributes(new Events<List<Attribute>>() {
 
-                getView().onFinishedFooter(contactEmails);
-            }
+			@Override
+			public void onFinished(List<Attribute> result) {
 
-            @Override
-            public void onError(PerunException error) {
-                getView().onFinishedFooter(null);
-            }
+				Attribute contactEmailAttr = null;
+				for (Attribute attr : result) {
+					if (attr.getFriendlyName().equals("contactEmail")) {
+						contactEmailAttr = attr;
+						break;
+					}
+				}
+				List<String> contactEmails = new ArrayList<>();
+				if (contactEmailAttr != null) {
+					contactEmails = Arrays.asList(contactEmailAttr.getValue().split(","));
+				}
 
-            @Override
-            public void onLoadingStart() {
-                getView().onLoadingStartFooter();
-            }
-        });
+				getView().onFinishedFooter(contactEmails);
+
+				String i = Window.Location.getParameter("i");
+				String m = Window.Location.getParameter("m");
+
+				if (i != null && m != null) {
+
+					PlaceRequest placeRequest = new PlaceRequest.Builder()
+							.nameToken(RegistrarPlaceTokens.VERIFY)
+							.with("i", i).with("m", m).build();
+					placeManager.revealPlace(placeRequest, false);
+
+				}
+
+			}
+
+			@Override
+			public void onError(PerunException error) {
+				getView().onFinishedFooter(null);
+			}
+
+			@Override
+			public void onLoadingStart() {
+				getView().onLoadingStartFooter();
+			}
+		});
 
 
 
-    }
+	}
 
+	private int resets = 0;
+	@Override
+	protected void onReset() {
+		super.onReset();
 
-    private int resets = 0;
-    @Override
-    protected void onReset() {
-        super.onReset();
+		// Because NavbarCollapse.hide() doesn't work properly. It shows the NavbarCollapse on application startup .
+		resets++;
+		if (resets > 2) {
+			getView().hideNavbar();
+		}
+	}
 
-        // Because NavbarCollapse.hide() doesn't work properly. It shows the NavbarCollapse on application startup .
-        resets++;
-        if (resets > 2) {
-            getView().hideNavbar();
-        }
-    }
+	private void loadVoAttributes(final Events<List<Attribute>> callback) {
 
-    private void loadVoAttributes(final Events<List<Attribute>> callback) {
+		final String voName = Window.Location.getParameter("vo");
 
-        final String voName = Window.Location.getParameter("vo");
-        final PerunPrincipal pp = PerunSession.getInstance().getPerunPrincipal();
+		if (voName == null || voName.isEmpty()) {
+			PerunException exception = PerunException.createNew("0", "WrongURL", "Missing parameters in URL.");
+			callback.onError(exception);
+			return;
+		}
 
-        if (voName == null || voName.isEmpty()) {
-            PerunException exception = PerunException.createNew("0", "WrongURL", "Missing parameters in URL.");
-            callback.onError(exception);
-            return;
-        }
+		RegistrarManager.getVoAndGroupAttributes(voName, null, new JsonEvents() {
 
-        RegistrarManager.getVoAndGroupAttributes(voName, null, new JsonEvents() {
+			@Override
+			public void onFinished(JavaScriptObject jso) {
+				List<Attribute> attributes = JsUtils.jsoAsList(jso);
+				callback.onFinished(attributes);
+			}
 
-            @Override
-            public void onFinished(JavaScriptObject jso) {
-                List<Attribute> attributes = JsUtils.jsoAsList(jso);
-                callback.onFinished(attributes);
-            }
+			@Override
+			public void onError(PerunException error) {
+				callback.onError(error);
+			}
 
-            @Override
-            public void onError(PerunException error) {
-                callback.onError(error);
-            }
+			@Override
+			public void onLoadingStart() {
+				callback.onLoadingStart();
+			}
+		});
 
-            @Override
-            public void onLoadingStart() {
-                callback.onLoadingStart();
-            }
-        });
-
-    }
+	}
 
 }
---
+++ perun-wui-registrar/src/main/java/cz/metacentrum/perun/wui/registrar/pages/VerifyEmailPresenter.java
@@ -0,0 +1,98 @@
+package cz.metacentrum.perun.wui.registrar.pages;
+
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.user.client.Window;
+import com.google.inject.Inject;
+import com.google.web.bindery.event.shared.EventBus;
+import com.gwtplatform.mvp.client.Presenter;
+import com.gwtplatform.mvp.client.View;
+import com.gwtplatform.mvp.client.annotations.NameToken;
+import com.gwtplatform.mvp.client.annotations.ProxyCodeSplit;
+import com.gwtplatform.mvp.client.proxy.PlaceManager;
+import com.gwtplatform.mvp.client.proxy.ProxyPlace;
+import com.gwtplatform.mvp.shared.proxy.PlaceRequest;
+import cz.metacentrum.perun.wui.client.PerunPresenter;
+import cz.metacentrum.perun.wui.client.resources.PerunSession;
+import cz.metacentrum.perun.wui.json.JsonEvents;
+import cz.metacentrum.perun.wui.json.managers.RegistrarManager;
+import cz.metacentrum.perun.wui.model.BasicOverlayObject;
+import cz.metacentrum.perun.wui.model.PerunException;
+import cz.metacentrum.perun.wui.registrar.client.RegistrarPlaceTokens;
+
+/**
+ * Presenter for displaying mail verification result
+ *
+ * @author Pavel Zlámal <zlamal@cesnet.cz>
+ */
+public class VerifyEmailPresenter extends Presenter<VerifyEmailPresenter.MyView, VerifyEmailPresenter.MyProxy> {
+
+	private PlaceManager placeManager = PerunSession.getPlaceManager();
+	private VerifyEmailPresenter presenter = this;
+
+	public interface MyView extends View {
+		void setResult(boolean result);
+		void onLoadingStart();
+		void onError(PerunException error, JsonEvents retry);
+	}
+
+	@NameToken(RegistrarPlaceTokens.VERIFY)
+	@ProxyCodeSplit
+	public interface MyProxy extends ProxyPlace<VerifyEmailPresenter> {
+	}
+
+	@Inject
+	public VerifyEmailPresenter(final EventBus eventBus, final MyView view, final MyProxy proxy) {
+		super(eventBus, view, proxy, PerunPresenter.SET_MAIN_CONTENT);
+	}
+
+	@Override
+	public void prepareFromRequest(final PlaceRequest request) {
+		super.prepareFromRequest(request);
+
+		try {
+			final String i = Window.Location.getParameter("i");
+			final String m = Window.Location.getParameter("m");
+			if (i == null || m == null) {
+				placeManager.revealErrorPlace(request.getNameToken());
+			}
+
+			RegistrarManager.validateEmail(i, m, new JsonEvents() {
+
+				final JsonEvents retry = this;
+
+				@Override
+				public void onFinished(JavaScriptObject jso) {
+					BasicOverlayObject result = jso.cast();
+					getView().setResult(result.getBoolean());
+					getProxy().manualReveal(presenter);
+				}
+
+				@Override
+				public void onError(PerunException error) {
+					if (error.getName().equals("PrivilegeException")) {
+						getProxy().manualRevealFailed();
+						placeManager.revealUnauthorizedPlace(request.getNameToken());
+					} else {
+						getProxy().manualRevealFailed();
+						placeManager.revealErrorPlace(request.getNameToken());
+						getView().onError(error, retry);
+					}
+				}
+
+				@Override
+				public void onLoadingStart() {
+					getView().onLoadingStart();
+				}
+			});
+		} catch( NumberFormatException e ) {
+			getProxy().manualRevealFailed();
+			placeManager.revealErrorPlace(request.getNameToken());
+		}
+
+	}
+
+	@Override
+	public boolean useManualReveal() {
+		return true;
+	}
+}
