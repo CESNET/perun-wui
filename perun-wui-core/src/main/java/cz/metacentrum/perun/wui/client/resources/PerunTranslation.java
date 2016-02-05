8c574a44816edb48cfaa75e3a8f4fac33206bd47
Pavel Zlámal <zlamal@cesnet.cz>
2016-02-05 13:07:29 +0100

    WUI: Added forgotten files to commit

--- perun-wui-core/src/main/java/cz/metacentrum/perun/wui/client/resources/PerunTranslation.java
+++ perun-wui-core/src/main/java/cz/metacentrum/perun/wui/client/resources/PerunTranslation.java
@@ -154,8 +154,11 @@ public interface PerunTranslation extends Messages {
 
 	/* ------------ FOOTER ---------------------------- */
 
-	@DefaultMessage("<i class=\"fa fa-support\">&nbsp;</i>Support at <a href=\"mailto:{0}\">{0}</a></strong>")
+	@DefaultMessage("<i class=\"fa fa-support\">&nbsp;</i>Support at <a href=\"mailto:{0}\">{0}</a>")
 	public String supportAt(String email);
+
+	@DefaultMessage("<i class=\"fa fa-support\">&nbsp;</i>Support at&nbsp;")
+	public String supportAtMails();
 
 	@DefaultMessage("Powered by <a href=\'https://perun.cesnet.cz/web/\'>Perun</a> &copy; {0} <a href=\'https://www.cesnet.cz/\'>CESNET</a> &amp; <a href=\'https://www.cerit-sc.cz/\'>CERIT-SC</a>")
 	public String credits(int year);
