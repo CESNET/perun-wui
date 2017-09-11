7934ff5fdd4d8bacdb34ef601a0cfdcbe5be23f1
Vojtěch Sassmann <vojtech.sassmann@gmail.com>
2017-09-11 13:17:16 +0200

    Edit modules versions
    * Set GWT version to 2.8.1, GWTP to 1.6 and GwtBootstrap3 to 0.9.4
    * Bootstrap version 3.3.7
    * JQuery version 1.12.4
    * In every *.gwt.xml files added property gssDefaultInUiBinder to true,
    so the GSS is used as default.
    * Added string constants in Select widget.

--- perun-wui-registrar/src/main/java/cz/metacentrum/perun/wui/registrar/widgets/Select.java
+++ perun-wui-registrar/src/main/java/cz/metacentrum/perun/wui/registrar/widgets/Select.java
@@ -37,14 +37,11 @@ import org.gwtbootstrap3.client.ui.base.mixin.AttributeMixin;
 import org.gwtbootstrap3.client.ui.base.mixin.EnabledMixin;
 import org.gwtbootstrap3.client.ui.base.mixin.FocusableMixin;
 import org.gwtbootstrap3.client.ui.constants.ButtonType;
-import org.gwtbootstrap3.extras.select.client.constants.Styles;
 import org.gwtbootstrap3.extras.select.client.ui.Option;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.gwtbootstrap3.extras.select.client.constants.DataAttributes.*;
-import static org.gwtbootstrap3.extras.select.client.constants.DataAttributes.DATA_STYLE;
 
 
 /**
@@ -59,12 +56,25 @@ public class Select extends ListBox implements Focusable, HasEnabled {
 	private static final String DESELECT_ALL = "deselectAll";
 	private static final String TRUE = "true";
 
+	private static final String BOOTSTRAP_SELECT = "bootstrap-select";
+	private static final String DATA_HEADER = "data-header";
+	private static final String DATA_SHOW_SUBTEXT = "data-show-subtext";
+	private static final String DATA_SIZE = "data-size";
+	private static final String DATA_WIDTH = "data-width";
+	private static final String DATA_SELECTED_TEXT_FORMAT = "data-selected-text-format";
+	private static final String DATA_LIVE_SEARCH = "data-live-search";
+	private static final String DATA_STYLE = "data-style";
+
+	private static final String SHOW_MENU_ARROW = "show-menu-arrow";
+	private static final String SHOW_TICK = "show-tick";
+	private static final String MULTIPLE = "multiple";
+
 	private final AttributeMixin<Select> attributeMixin = new AttributeMixin<Select>(this);
 	private final FocusableMixin<Select> focusableMixin = new FocusableMixin<Select>(this);
 	private final EnabledMixin<Select> enabledMixin = new EnabledMixin<Select>(this);
 
 	public Select() {
-		setStyleName(Styles.BOOTSTRAP_SELECT);
+		setStyleName(BOOTSTRAP_SELECT);
 	}
 
 	public Select(boolean isMultipleSelect) {
@@ -167,26 +177,26 @@ public class Select extends ListBox implements Focusable, HasEnabled {
 
 	public void setShowMenuArrow(final boolean showMenuArrow) {
 		if (showMenuArrow) {
-			addStyleName(Styles.SHOW_MENU_ARROW);
+			addStyleName(SHOW_MENU_ARROW);
 		} else {
-			removeStyleName(Styles.SHOW_MENU_ARROW);
+			removeStyleName(SHOW_MENU_ARROW);
 		}
 	}
 
 	public boolean getShowMenuArrow() {
-		return StyleHelper.containsStyle(getStyleName(), Styles.SHOW_MENU_ARROW);
+		return StyleHelper.containsStyle(getStyleName(), SHOW_MENU_ARROW);
 	}
 
 	public void setShowTick(final boolean showTick) {
 		if (showTick) {
-			addStyleName(Styles.SHOW_TICK);
+			addStyleName(SHOW_TICK);
 		} else {
-			removeStyleName(Styles.SHOW_TICK);
+			removeStyleName(SHOW_TICK);
 		}
 	}
 
 	public boolean getShowTick() {
-		return StyleHelper.containsStyle(getStyleName(), Styles.SHOW_TICK);
+		return StyleHelper.containsStyle(getStyleName(), SHOW_TICK);
 	}
 
 	/**
@@ -409,7 +419,7 @@ public class Select extends ListBox implements Focusable, HasEnabled {
 
 	private native void initialize(Element e) /*-{
 
-		var elem = $wnd.jQuery(e);
+	var elem = $wnd.jQuery(e);
 
 		elem.selectpicker({
 			iconBase: 'fa',
@@ -417,7 +427,6 @@ public class Select extends ListBox implements Focusable, HasEnabled {
 			style: 'btn-default form-control',
 			dropupAuto: true
 		});
-
 	}-*/;
 
 	private native void setValue(Element e, JsArrayString value) /*-{
