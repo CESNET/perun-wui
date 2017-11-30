3a8091eed049e6844ce1aa326fabccad1c255984
Pavel Zlámal <zlamal@cesnet.cz>
2017-11-30 09:21:47 +0100

    WUI: Allow regex check on radio buttons on registration forms
    - There was a missing check on existing regex for radio buttons.
      There seems to be check only on input fields and text area like
      widgets.

--- perun-wui-registrar/src/main/java/cz/metacentrum/perun/wui/registrar/widgets/items/validators/RadioboxValidator.java
+++ perun-wui-registrar/src/main/java/cz/metacentrum/perun/wui/registrar/widgets/items/validators/RadioboxValidator.java
@@ -1,5 +1,7 @@
 package cz.metacentrum.perun.wui.registrar.widgets.items.validators;
 
+import com.google.gwt.regexp.shared.MatchResult;
+import com.google.gwt.regexp.shared.RegExp;
 import cz.metacentrum.perun.wui.registrar.widgets.items.Radiobox;
 import org.gwtbootstrap3.client.ui.constants.ValidationState;
 
@@ -15,6 +17,21 @@ public class RadioboxValidator extends PerunFormItemValidatorImpl<Radiobox> {
 			setResult(Result.EMPTY);
 			radiobox.setStatus(getTransl().cantBeEmpty(), ValidationState.ERROR);
 			return false;
+		}
+
+		String regex = radiobox.getItemData().getFormItem().getRegex();
+
+		if (regex != null && !regex.equals("")) {
+
+			RegExp regExp = RegExp.compile(regex);
+			MatchResult matcher = regExp.exec(radiobox.getValue());
+			boolean matchFound = (matcher != null); // equivalent to regExp.test(inputStr);
+			if(!matchFound){
+
+				setResult(Result.INVALID_FORMAT);
+				radiobox.setStatus(getErrorMsgOrDefault(radiobox), ValidationState.ERROR);
+				return false;
+			}
 		}
 
 		radiobox.setStatus(ValidationState.SUCCESS);
