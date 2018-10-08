40faac75e092c54d2f9e48fe83ab7b91fffb20bc
Pavel Zlámal <zlamal@cesnet.cz>
2018-10-08 11:17:57 +0200

    WUI: Refresh group initial form
    - When anonymous person submits vo registration with automatic
      approval, user is created in Perun. When he continues with
      registration to group, this information is not reflected on the form,
      since it was loaded before.
    - Now we reload group form during the process, hence form items can be
      pre-filled with data from Perun (submitted vo form) and group form
      is submitted with user id set.
    - Fixed displaying vo logo.
    - Added methods to set back form and exception into registrar object.

--- perun-wui-registrar/src/main/java/cz/metacentrum/perun/wui/registrar/pages/steps/GroupInitStep.java
+++ perun-wui-registrar/src/main/java/cz/metacentrum/perun/wui/registrar/pages/steps/GroupInitStep.java
@@ -1,6 +1,10 @@
 package cz.metacentrum.perun.wui.registrar.pages.steps;
 
+import com.google.gwt.core.client.JavaScriptObject;
 import cz.metacentrum.perun.wui.json.Events;
+import cz.metacentrum.perun.wui.json.JsonEvents;
+import cz.metacentrum.perun.wui.json.managers.AuthzManager;
+import cz.metacentrum.perun.wui.json.managers.RegistrarManager;
 import cz.metacentrum.perun.wui.model.PerunException;
 import cz.metacentrum.perun.wui.model.beans.Application;
 import cz.metacentrum.perun.wui.model.common.PerunPrincipal;
@@ -14,6 +18,8 @@ import cz.metacentrum.perun.wui.registrar.widgets.PerunForm;
  */
 public class GroupInitStep extends FormStep {
 
+	PerunPrincipal localPP;
+
 	public GroupInitStep(RegistrarObject registrar, PerunForm form) {
 		super(registrar, form);
 		this.result = new Result(Type.GROUP_INIT, registrar.getGroup(), registrar.hasGroupFormAutoApproval());
@@ -23,6 +29,46 @@ public class GroupInitStep extends FormStep {
 	public void call(final PerunPrincipal pp, Summary summary, final Events<Result> events) {
 
 		events.onLoadingStart();
+		this.localPP = pp;
+
+		// if there is a VoInitForm then there should be previous step where user submitted VO init application
+		// it it`s in an auto-approval mode, then User might have been created and we want to reflect this in
+		// group application, hence we will forcefully reload its form and session info.
+		if (!registrar.getVoFormInitial().isEmpty() &&
+				registrar.getVoFormInitialException() == null &&
+				registrar.hasVoFormAutoApproval()) {
+
+			reloadGroupAppData(new JsonEvents() {
+				@Override
+				public void onFinished(JavaScriptObject result) {
+					// continue after all is loaded
+					continueWithForm(events);
+				}
+
+				@Override
+				public void onError(PerunException error) {
+
+				}
+
+				@Override
+				public void onLoadingStart() {
+
+				}
+			});
+		} else {
+			// no group reloading
+			continueWithForm(events);
+		}
+
+	}
+
+	/**
+	 * Finish form processing (previously part of call() method).
+	 * Called after group form is refreshed.
+	 *
+	 * @param events events of original call to continue with
+	 */
+	private void continueWithForm(final Events<Result> events) {
 
 		if (registrar.getGroupFormInitialException() != null) {
 			result.setException(registrar.getGroupFormInitialException());
@@ -40,11 +86,60 @@ public class GroupInitStep extends FormStep {
 		}
 
 		form.setApp(Application.createNew(registrar.getVo(), registrar.getGroup(), Application.ApplicationType.INITIAL,
-				getFedInfo(pp), pp.getActor(), pp.getExtSource(), pp.getExtSourceType(), pp.getExtSourceLoa(), pp.getUser()));
+				getFedInfo(localPP), localPP.getActor(), localPP.getExtSource(), localPP.getExtSourceType(), localPP.getExtSourceLoa(), localPP.getUser()));
 
 		form.setOnSubmitEvent(getOnSubmitEvent(events));
 
 		form.performAutoSubmit();
+
+	}
+
+	/**
+	 * In some cases we want to reload form data
+	 */
+	private void reloadGroupAppData(JsonEvents events) {
+
+		AuthzManager.getPerunPrincipal(new JsonEvents() {
+			@Override
+			public void onFinished(JavaScriptObject result) {
+				localPP = result.cast();
+
+				// since this is a group initi form we know that vo and group are not null.
+				RegistrarManager.initializeRegistrar(registrar.getVo().getShortName(), registrar.getGroup().getName(), new JsonEvents() {
+					@Override
+					public void onFinished(JavaScriptObject result) {
+						RegistrarObject ro = result.cast();
+						registrar.setGroupFormInitial(ro.getGroupFormInitial());
+						registrar.setGroupFormInitialException(ro.getGroupFormInitialException());
+						// continue with the step
+						events.onFinished(null);
+					}
+
+					@Override
+					public void onError(PerunException error) {
+						// ignore and continue with the step
+						events.onFinished(null);
+					}
+
+					@Override
+					public void onLoadingStart() {
+					}
+				});
+
+			}
+
+			@Override
+			public void onError(PerunException error) {
+				// ignore it and continue with default form init processing
+				events.onFinished(null);
+			}
+
+			@Override
+			public void onLoadingStart() {
+			}
+		});
+
+
 	}
 
 }
