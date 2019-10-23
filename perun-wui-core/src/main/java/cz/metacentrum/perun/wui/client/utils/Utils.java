cf6181cf39df9a91bc00717c1136fa9999e92c7e
Pavel Zlámal <zlamal@cesnet.cz>
2019-10-23 10:41:50 +0200

    WUI: Regex for mail shouldn't allow upper-cased domain name and TLD.

--- perun-wui-core/src/main/java/cz/metacentrum/perun/wui/client/utils/Utils.java
+++ perun-wui-core/src/main/java/cz/metacentrum/perun/wui/client/utils/Utils.java
@@ -1315,7 +1315,7 @@ public class Utils {
 		//var reg1 = /(@.*@)|(\.\.)|(@\.)|(\.@)|(^\.)/; // not valid
 		//var reg2 = /^.+\@(\[?)[a-zA-Z0-9\-\.]+\.([a-zA-Z]{2,3}|[0-9]{1,3})(\]?)$/; // valid
 		//return !reg1.test(email) && reg2.test(email);
-		var reg4 = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+		var reg4 = /^[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
 		return reg4.test(email);
 	}-*/;
 
