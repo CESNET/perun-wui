92dd20ef8a99945d0aa423c30c074af24aa9b1db
Pavel Zlámal <zlamal@cesnet.cz>
2023-11-06 07:32:22 +0100

    fix: use new egi checkin entityid in translations

--- perun-wui-core/src/main/java/cz/metacentrum/perun/wui/client/utils/Utils.java
+++ perun-wui-core/src/main/java/cz/metacentrum/perun/wui/client/utils/Utils.java
@@ -133,7 +133,8 @@ public class Utils {
 		//organizationsTranslation.put("https://login.e-infra.cz/idp/", "e-INFRA CZ AAI");
 
 		// EGI
-		organizationsTranslation.put("https://aai.egi.eu/proxy/saml2/idp/metadata.php", "EGI Check-In");
+		organizationsTranslation.put("https://aai.egi.eu/proxy/saml2/idp/metadata.php", "EGI CheckIn");
+		organizationsTranslation.put("https://aai.egi.eu/auth/realms/egi", "EGI CheckIn"); // keycoak based
 		organizationsTranslation.put("https://sso.egi.eu/edugainidp/shibboleth", "EGI Foundation");
 		organizationsTranslation.put("https://sso.egi.eu/egissoidp/shibboleth", "EGI SSO");
 		organizationsTranslation.put("https://www.egi.eu/idp/shibboleth", "EGI SSO");
